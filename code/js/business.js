4bc792dcb9391a5c6d4f8b0e426ee54f14fda3d9
Alberto Pacheco <albertovazpacheco@hotmail.com>
2019-05-07 18:32:20 +0200

    fetching done

---
+++ code/js/business.js
@@ -0,0 +1,153 @@
+let urlParams = new URLSearchParams(window.location.search);
+const database = firebase.database();
+let catId = urlParams.get("category");
+let urlKey = urlParams.get("key");
+const businessName = document.querySelector("[data-name]");
+const businessType = document.querySelector("[data-type]");
+const businessImage = document.querySelector("[data-image]");
+const businessLongDesc = document.querySelector("[data-long_desc]");
+const businessLocationMaps = document.querySelectorAll(".gmap_link");
+
+const Business = {
+  key: "",
+  name: "",
+  type: "",
+  shortDescription: "",
+  longDescription: "",
+  location: "",
+  image: "assets/img/small/",
+  price: "",
+  distance: "",
+  contact: {
+    fblink: "",
+    telephone: "",
+    weblink: "",
+    instaLink: "",
+    businessAdress: ""
+  },
+  badges: {
+    ratable: [
+      {
+        name: "vegetarian",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "vegan",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "localProducts",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "plasticFree",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "organic",
+        rate: 0,
+        votes: 0
+      }
+    ],
+    notRatable: [
+      {
+        name: "zeroWaste",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "secondHand",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "recycled",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "ethical",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "cleanEnergy",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "energySaving",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "co2Emissions",
+        rate: 0,
+        votes: 0
+      }
+    ]
+  },
+  filtersArray: [],
+
+  createBadges(filters) {
+    console.log(filters.ratable, filters.notRatable);
+    let full_badges = filters.ratable.concat(filters.notRatable);
+    console.log(full_badges);
+
+    full_badges.forEach(filter => {
+      averageRate = filter.rate / filter.votes;
+
+      console.log(averageRate, filter.name);
+      if (averageRate > 5) {
+        console.log(averageRate, filter.name);
+        this.filtersArray.push(filter.name);
+      }
+    });
+  }
+};
+
+function init() {
+  buildBusiness();
+}
+
+function buildBusiness() {
+  database.ref(catId + "/").on("child_added", snapshot => {
+    const key = snapshot.key;
+    const data = snapshot.val();
+    console.log(key);
+    console.log(data);
+    if (urlKey == key) {
+      const business = Object.create(Business);
+      business.key = key;
+      business.name = data.name;
+      business.type = data.type;
+      business.shortDescription = data.shortDescription;
+      business.longDescription = data.longDescription;
+      business.location = data.location;
+      business.image = data.image;
+      business.contact = data.contact;
+      business.price = data.price;
+      business.distance = data.distance;
+      business.badges = data.badges;
+      business.filtersArray = [];
+
+      business.createBadges(data.badges);
+      showBusiness(business);
+    }
+  });
+}
+
+function showBusiness(business) {
+  businessName.textContent = business.name;
+  businessType.textContent = business.type;
+  businessLongDesc.textContent = business.longDescription;
+  businessLocationMaps.forEach(gmap => {
+    gmap.src = business.location;
+  });
+}
+
+init();
---
+++ code/js/gallery.js
@@ -0,0 +1,164 @@
+let urlParams = new URLSearchParams(window.location.search);
+const database = firebase.database();
+let catId = urlParams.get("category");
+const gallery = document.querySelector(".gallery__container");
+const filterList = document.querySelector(".filter__list");
+
+let listOfBusinesses = [];
+
+const Business = {
+  key: "",
+  name: "",
+  type: "",
+  shortDescription: "",
+  longDescription: "",
+  location: "",
+  image: "assets/img/small/",
+  price: "",
+  distance: "",
+  contact: {
+    fblink: "",
+    telephone: "",
+    weblink: "",
+    instaLink: "",
+    businessAdress: ""
+  },
+  badges: {
+    ratable: [
+      {
+        name: "vegetarian",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "vegan",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "localProducts",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "plasticFree",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "organic",
+        rate: 0,
+        votes: 0
+      }
+    ],
+    notRatable: [
+      {
+        name: "zeroWaste",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "secondHand",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "recycled",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "ethical",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "cleanEnergy",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "energySaving",
+        rate: 0,
+        votes: 0
+      },
+      {
+        name: "co2Emissions",
+        rate: 0,
+        votes: 0
+      }
+    ]
+  },
+  filtersArray: [],
+
+  createBadges(filters) {
+    console.log(filters.ratable, filters.notRatable);
+    let full_badges = filters.ratable.concat(filters.notRatable);
+    console.log(full_badges);
+
+    full_badges.forEach(filter => {
+      averageRate = filter.rate / filter.votes;
+
+      console.log(averageRate, filter.name);
+      if (averageRate > 5) {
+        console.log(averageRate, filter.name);
+        this.filtersArray.push(filter.name);
+      }
+    });
+  }
+};
+
+function init() {
+  createObject();
+}
+
+function createObject() {
+  database.ref(catId + "/").on("child_added", snapshot => {
+    const key = snapshot.key;
+    const data = snapshot.val();
+
+    const business = Object.create(Business);
+    business.key = key;
+    business.name = data.name;
+    business.type = data.type;
+    business.shortDescription = data.shortDescription;
+    business.image = data.image;
+    business.location = data.location;
+    business.badges = data.badges;
+    business.filtersArray = [];
+    business.createBadges(data.badges);
+
+    pushBusinessToList(business);
+  });
+}
+
+function pushBusinessToList(business) {
+  listOfBusinesses.push(business);
+  displayListBusiness(business);
+}
+
+function displayListBusiness(business) {
+  let clone = document
+    .querySelector(".gallery__item__template")
+    .content.cloneNode(true);
+
+  clone.querySelector("[data-link_to_subpage]").href =
+    "business.html?category=" + catId + "&key=" + business.key;
+  clone.querySelector("[data-image]").textContent = "";
+  clone.querySelector("[data-name]").textContent = business.name;
+  clone.querySelector("[data-type]").textContent = business.type;
+  clone.querySelector("[data-short_desc]").textContent =
+    business.shortDescription;
+  //clone.querySelector("[data-price]")
+  clone.querySelector("[data-distance]").textContent = business.distance;
+  let badgesList = clone.querySelector("[data-badges_container]");
+  business.filtersArray.forEach(filter => {
+    // let li = document.createElement("li");
+    // li.textContent = filter;
+    // console.log(li);
+    // badgesList.appendChild(li);
+  });
+  gallery.appendChild(clone);
+}
+
+init();
