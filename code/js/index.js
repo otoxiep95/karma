0413dd2d82f93bf24489c46afc32bfc397fa4b70
Alberto Pacheco <albertovazpacheco@hotmail.com>
2019-05-13 21:48:26 +0200

    going back and about nav

--- code/js/index.js
+++ code/js/index.js
@@ -1,15 +1,29 @@
 "use strict";
-
-let letsGo = document.querySelector(".launching__button-link");
-let menuMain = document.querySelector(".menu-main");
-
+let urlParams = new URLSearchParams(window.location.search);
+let screenFlag = urlParams.get("interaction");
+const letsGo = document.querySelector(".launching__button-link");
+const menuMain = document.querySelector(".menu-main");
+const launchingScreen = document.querySelector(".launching");
+const aboutScreen = document.querySelector(".about-main");
 window.addEventListener("DOMContentLoaded", initIndex);
 
 function initIndex() {
   letsGo.addEventListener("click", openMainMenu);
+  if (screenFlag === "menu") {
+    openMainMenu();
+  }
+  if (screenFlag === "about") {
+    openAbout();
+  }
 }
 
 function openMainMenu() {
   console.log("open menuMain");
   menuMain.style.display = "grid";
+}
+
+function openAbout() {
+  launchingScreen.style.display = "none";
+  //aboutScreen.classList.remove("desk-only");
+  aboutScreen.style.display = "grid";
 }
